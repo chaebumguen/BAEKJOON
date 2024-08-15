eeb80710edff4434e8abf8c688f806a03aa7a5eb
BreadCrab <chaebumguen@gmail.com>
2024-08-15 15:44:04 +0900

    32025번 문제 해결

---
+++ Playground is Mathematic/Playground is Mathematic/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Playground_is_Mathematic.Calculate;
+
+namespace Playground_is_Mathematic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a;
+
+            int H = Convert.ToInt32(Console.ReadLine());
+            Condition(H);
+
+            int W = Convert.ToInt32(Console.ReadLine());
+            Condition(W);
+
+            a = (CalculationRcm(SelectSmallNum(H, W)));
+
+            Console.WriteLine(a);
+        }
+    }
+
+    class Calculate
+    {
+        public static void Condition(int Meter)
+        {
+            if (Meter < 5 || Meter > 1000)
+            {
+                return;
+            }
+        }
+
+        public static int SelectSmallNum(int a, int b)
+        {
+            int number = a;
+
+            if (a > b)
+            {
+                number = b;
+
+                return number;
+            }
+            else
+            {
+                return number;
+            }
+        }
+
+        public static double CalculationRcm(int a)
+        {
+            double Width = 100 * (Convert.ToDouble(a) / 2);
+
+            return Width;
+        }
+    }
+}
